30c78a01840eea8c5ac8432558ffd30dae238fb6
CPEFPA <adzoagbavon@gmail.com>
2026-05-13 01:03:02 +0000

    🔧 firebase-config.js : export auth ajouté

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,13 +1,7 @@
-﻿// 🔥 js/firebase-config.js - Configuré pour dedieno-demo
+﻿// 🔥 js/firebase-config.js - Version Auth + Export complet
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
-import { 
-  getDatabase, 
-  ref, 
-  set, 
-  onValue, 
-  push, 
-  update 
-} from "https://www.gstatic.com/firebasejs/10.12.0/firebase-database.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-database.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBhlLLm1Vvl3U3JYFZ8rzJJW4pimYDQEEU",
@@ -22,7 +16,7 @@ const firebaseConfig = {
 // Initialisation
 export const app = initializeApp(firebaseConfig);
 export const db = getDatabase(app);
+export const auth = getAuth(app);
 
 // Export des fonctions utiles
-export { ref, set, onValue, push, update };
-
+export { ref, set, onValue, push, update } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-database.js";
